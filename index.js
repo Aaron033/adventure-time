663189b1f22e5501ca1f881e11060fdddc9a4606
Patricia Arbona <patricia.arbona@omadahealth.com>
2017-01-16 00:56:17 -0800

    OAuth Handshake

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const moment = require('moment');
 const titleize = require('titleize');
+const request = require('request');
 
 const app = express();
 app.use(bodyParser.json()); //support json encoded bodies
@@ -15,6 +16,37 @@ const server = app.listen(3000, () => {
   console.log('Express server is listening on part %d in %s mode', server.address().port, app.settings.env);
 });
 
+// Authorization handshake
+
+app.get('/slack', function(req, res){
+  if (!req.query.code) { // access denied
+    res.redirect('www.patriciaarbona.com');
+    return;
+  }
+  var data = {form: {
+      client_id: process.env.SLACK_CLIENT_ID,
+      client_secret: process.env.SLACK_CLIENT_SECRET,
+      code: req.query.code
+  }};
+  request.post('https://slack.com/api/oauth.access', data, function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      // Get an auth token
+      let token = JSON.parse(body).access_token;
+
+      // Get the team domain name to redirect to the team URL after auth
+      request.post('https://slack.com/api/team.info', {form: {token: token}}, function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+          if(JSON.parse(body).error == 'missing_scope') {
+            res.send('HTTP Status Cats has been added to your team!');
+          } else {
+            let team = JSON.parse(body).team.domain;
+            res.redirect('http://' +team+ '.slack.com');
+          }
+        }
+      });
+    }
+  })
+});
 
 //HTTP POST route method
 app.post('/', (req, res) => {
